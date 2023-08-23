f12ef6c9f4776ecef79a0e07c3f901ab4a21fa42
Will Hernandez <w.hernandez.code@gmail.com>
2023-08-23 14:41:18 -0400

    redis cache redo canceled. only image files atm

--- routes/tests/user_routes.test.js
+++ routes/tests/user_routes.test.js
@@ -1,15 +1,7 @@
 const supertest = require('supertest')
 const backend_url = 'http://localhost:4000'
 
-// if passing the backend url into supertest, run server prior so tests can attach
-// const response = await supertest("http://localhost:4000").get('/api')
-// benefits of using the url rather than the app objects is we can debug our tests
-
-// if using app, do NOT run server prior as a new one will be created and attached to
-// const app = require('../server')
-// const response = await supertest(app).get('/api')
-
-describe("Permission - AWS IAM api requests", () => {
+describe("User routes / permissions - AWS IAM api requests", () => {
 	const data = {
 		username: `testUser_${Math.floor(Math.random()*898)+101}@tester.com`
 	}
@@ -24,13 +16,18 @@ describe("Permission - AWS IAM api requests", () => {
 		expect(response.status).toEqual(409);
 	})
 
-	it('Logs in to the backend api/user/login : Status 200', async () => {
-		const response = await supertest(backend_url).post('/api/user/login').send(data)
+	it('Logs user in to the backend api/user/login : Status 200', async () => {
+		const response = await supertest(backend_url).post('/api/user/login')
 		expect(response.status).toEqual(200);
 	})
 
 	it('Deletes an AWS IAM user api/user/deleteuser : Status 200', async () => {
 		const response = await supertest(backend_url).post('/api/user/deleteuser').send(data)
+		expect(response.status).toEqual(200);
+	})
+
+	it('Logs user out of the backend api/user/logout : Status 200', async () => {
+		const response = await supertest(backend_url).get('/api/user/logout')
 		expect(response.status).toEqual(200);
 	})
 })
