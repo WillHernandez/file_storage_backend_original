4773b2a9b0621e9a824334d73a1ab16d2c966b1b
Will Hernandez <w.hernandez.code@gmail.com>
2023-08-30 12:11:02 -0400

    Added Test route before deploy

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }))
 
+app.use('/api/test', (req, res) => res.status(200).json('Test Successful!'))
 app.use('/api/user', userRoutes)
 app.use('/api/bucket', S3Routes)
 
