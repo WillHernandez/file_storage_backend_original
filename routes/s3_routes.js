bf6bf582d78706e240b7a4819bf173305bc97aff
Will Hernandez <w.hernandez.code@gmail.com>
2023-09-05 18:19:20 -0400

    added length check redis arr

--- routes/s3_routes.js
+++ routes/s3_routes.js
@@ -4,7 +4,7 @@ const multerMiddleWare = require('../middlewares/multerMiddleware.js')
 const convertHeic = require('../middlewares/convert_heic')
 const assumeRole = require('../middlewares/assume_role.js')
 const { getRedisCache } = require('../middlewares/redis_cache.js')
-const validateToken = require('../middlewares/validateToken.js')
+// const validateToken = require('../middlewares/validateToken.js')
 
 const { uploadObjects, deleteObjects, getPresignedUrls } = require('../controllers/s3_controllers.js')
 
